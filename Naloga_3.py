6975c81bfd15116afe4f8d6b06ff34adc4483004
Vid-Zganjar <147034349+Vid-Zganjar@users.noreply.github.com>
2023-10-08 14:46:15 +0200

    Add files via upload

---
+++ Naloga_3.py
@@ -0,0 +1,13 @@
+a=input("Vpisi 1 vrednost: ")
+b=input("Vpisi 2 vrednost: ")
+c=input("Vpisi 3 vrednost: ")
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+
+if(a==b):
+    print("prva vrednost je enaka drugi")
+
+if(int(c)<=int(a)):
+    print("tretja vrednost je manjsa ali enaka prvi")
